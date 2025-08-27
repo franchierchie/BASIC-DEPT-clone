effb40ba8bce453530506fa636b22b4f218d038e
franchierchie <franchierchie@gmail.com>
2025-08-27 19:06:42 -0300

    navbar

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image"
+import Link from "next/link"
+import { BsThreeDots } from "react-icons/bs"
 
 export const NavBar = () => {
   return (
-    <nav className="py-15 px-22">
-      <div> {/* LOGO */}
+    <nav className="flex items-center justify-between py-14 px-22">
+      <div className="min-w-fit"> {/* LOGO */}
         <Image
           src="/logos/basic-dept-logo.svg"
           alt="BASIC/DEPT logo"
@@ -13,6 +15,26 @@ export const NavBar = () => {
           unoptimized
         />
       </div>
+
+      <nav className="lg:w-4xl transition-all"> {/* NAVIGATION */}
+        <ul className="hidden 2xl:flex items-center lg:gap-18 uppercase font-bold">
+          <li><Link href="/">Work</Link></li>
+          <li><Link href="/">About</Link></li>
+          <li><Link href="/">News</Link></li>
+          <li><Link href="/">Thinking</Link></li>
+          <li><Link href="/">Careers</Link></li>
+          <li><Link href="/">Contact</Link></li>
+        </ul>
+      </nav>
+
+      <button> {/* MENU */}
+        <div className="hidden 2xl:block">
+          <BsThreeDots size={ 30 } />
+        </div>
+        <div className="block 2xl:hidden">
+          <p className="uppercase font-bold text-lg">Menu</p>
+        </div>
+      </button>
     </nav>
   )
 }
